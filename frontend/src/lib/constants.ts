3804b3e9d682d0e721946ec2f47acb042902501c
hasan <ha5anmukhli50@gmail.com>
2022-04-10 11:05:15 +0800

    updated img source

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,1 +1,1 @@
-export const imgSrc = "https://raw.githubusercontent.com/hazelnutcloud/legions-of-oasis-v1/master/sveltekit/static/"
+export const imgSrc = "https://raw.githubusercontent.com/hazelnutcloud/legions-of-oasis/master/sveltekit/static/"
