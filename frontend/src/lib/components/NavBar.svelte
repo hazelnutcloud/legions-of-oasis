457d81ecee8b472fd0dbfd862847d9dc861ff22e
hasan <ha5anmukhli50@gmail.com>
2022-04-13 11:47:50 +0800

    added shadows to buttons

--- frontend/src/lib/components/NavBar.svelte
+++ frontend/src/lib/components/NavBar.svelte
@@ -17,7 +17,7 @@
   }
 </script>
 
-<div class="w-full navbar bg-base-300 h-16 sticky top-0 left-0 z-50">
+<div class="w-full navbar bg-base-300 h-16 sticky top-0 left-0 z-50 shadow-md">
   <div class="flex-none lg:hidden">
     <label for="my-drawer-3" class="btn btn-square btn-ghost">
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
